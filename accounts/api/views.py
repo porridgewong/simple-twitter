35b2d59b37381fdfff6db8b7aea4dba4fe260e5a
porridgewong <stephenwong.rs@gmail.com>
2021-05-31 16:17:51 -0700

    Add an API to check user login status

--- accounts/api/views.py
+++ accounts/api/views.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.models import User
 from rest_framework import viewsets
 from rest_framework import permissions
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from accounts.api.serializers import UserSerializer
 
 
@@ -11,3 +13,12 @@ class UserViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = User.objects.all().order_by('-date_joined')
     serializer_class = UserSerializer
     permission_classes = [permissions.IsAuthenticated]
+
+
+class AccountViewSet(viewsets.ViewSet):
+    @action(methods=['GET'], detail=False)
+    def login_status(self, request):
+        data = {'has_logged_in': request.user.is_authenticated}
+        if request.user.is_authenticated:
+            data['user'] = UserSerializer(request.user).data
+        return Response(data)
