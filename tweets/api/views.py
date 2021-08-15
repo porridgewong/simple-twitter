4b1a57127649d53fb24944101a0d8cb72cc07633
porridgewong <stephenwong.rs@gmail.com>
2021-08-15 13:45:02 +0800

    add a retrieve api for Tweets. It retrieve a tweet with comments. Use a decorator to validate the given query parameters.

--- tweets/api/views.py
+++ tweets/api/views.py
@@ -1,27 +1,32 @@
-from rest_framework import viewsets
-from rest_framework import permissions
-from rest_framework.response import Response
-from tweets.api.serializers import TweetSerializer, TweetSerializerForCreate
-from tweets.models import Tweet
 from newsfeeds.services import NewsfeedService
+from rest_framework import permissions
+from rest_framework import viewsets
+from rest_framework.response import Response
+from tweets.api.serializers import TweetSerializer, TweetSerializerForCreate, TweetSerializerWithComments
+from tweets.models import Tweet
+from util.decorators import required_params
 
 
 class TweetViewSet(viewsets.GenericViewSet):
     serializer_class = TweetSerializerForCreate
+    queryset = Tweet.objects.all()
 
     def get_permissions(self):
-        if self.action == 'list':
+        if self.action in ['list', 'retrieve']:
             return [permissions.AllowAny()]
         return [permissions.IsAuthenticated()]
 
-    def list(self, request):
-        if 'user_id' not in request.query_params:
-            return Response('missing user_id.', 400)
+    def retrieve(self, request, *args, **kwargs):
+        tweet = self.get_object()
+        return Response(TweetSerializerWithComments(tweet).data)
+
+    @required_params(params=['user_id'])
+    def list(self, request, *args, **kwargs):
         tweets = Tweet.objects.filter(user_id=request.query_params['user_id']).order_by('-created_at')
         serializers = TweetSerializer(tweets, many=True)
         return Response({'tweets': serializers.data})
 
-    def create(self, request):
+    def create(self, request, *args, **kwargs):
         serializer = TweetSerializerForCreate(data=request.data, context={'request': request})
         if not serializer.is_valid():
             return Response({
